6af53e67945990b2b10f7cece22cd5910f515365
cgk95 <kjkj5868@gmail.com>
2024-04-23 17:41:59 +0900

    feat :: 사용자별로 사용률을 제한하도록 RateLimiterConfig 변경

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 @Configuration
 public class RateLimiterConfig {
     @Bean
-    public KeyResolver keyResolver(){
-        return exchange -> Mono.just("anonymous");
+    public KeyResolver keyResolver() {
+        return exchange ->
+                exchange.getPrincipal() // 현재 인증된 사용자(프린시플)를 현재 요청에서 가져온다
+                        .map(Principal::getName) // 프린시플로부터 유저명을 추출
+                        .defaultIfEmpty("anonymous"); // 요청이 인증되지 않았다면 기본 키 값을 사용
+
     }
 }
