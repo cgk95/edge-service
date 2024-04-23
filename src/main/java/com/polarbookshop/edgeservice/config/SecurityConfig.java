8f8cc44227664d5f7dcfbdf82db9faa54542da0b
cgk95 <kjkj5868@gmail.com>
2024-04-23 19:50:47 +0900

    feat :: CSRF 처리 추가

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -11,23 +11,41 @@ import org.springframework.security.oauth2.client.registration.ReactiveClientReg
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
         http
-                .authorizeExchange(exchange -> exchange.pathMatchers("/","/*.css","/*.js","/favicon.ico").permitAll() // SPA의 정적 리소스에 대한 인증되지 않은 액세스 허용
-                        .pathMatchers(HttpMethod.GET,"/books/**").permitAll() // 카탈로그의 도서에 대한 인증되지 않은 액세스 허용
+                .authorizeExchange(exchange -> exchange.pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll() // SPA의 정적 리소스에 대한 인증되지 않은 액세스 허용
+                        .pathMatchers(HttpMethod.GET, "/books/**").permitAll() // 카탈로그의 도서에 대한 인증되지 않은 액세스 허용
                         .anyExchange().authenticated())
                 .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(
                         new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
                 ))
                 .oauth2Login(Customizer.withDefaults()) // OAuth2/OIDC를 사용한 사용자 인증을 활성화한다
                 .logout(logout -> logout.logoutSuccessHandler(
-                        oidcLogoutSuccessHandler(clientRegistrationRepository)));
+                        oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrf -> csrf.csrfTokenRepository(
+                        CookieServerCsrfTokenRepository.withHttpOnlyFalse()) // 프론트앤드와 CSRF 토큰을 교환하기 위해 쿠키 기반 방식을 사용
+                );
 
         return http.build();
+    }
+
+    @Bean
+    WebFilter csrfWebFilter() { // CSRF 리액티브 스트림을 구독하고 토큰의 값을 올바르게 추출하기 위한 목적만을 갖는 필터
+        return (exchange, chain) -> {
+            exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
+                Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
+                return csrfToken != null ? csrfToken.then() : Mono.empty();
+            }));
+            return chain.filter(exchange);
+        };
     }
 
     private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(ReactiveClientRegistrationRepository clientRegistrationRepository) {
@@ -37,4 +55,6 @@ public class SecurityConfig {
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
         return oidcLogoutSuccessHandler;
     }
+
+
 }
