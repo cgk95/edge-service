38d9f75d757ed8b993afba432183d4857da1c2a9
cgk95 <kjkj5868@gmail.com>
2024-04-23 19:38:05 +0900

    feat :: spa 어플리케이션에 대한 API Gateway route 추가, 스프링 시큐리티 인증설정 변경

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -1,22 +1,31 @@
 package com.polarbookshop.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
         http
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .authorizeExchange(exchange -> exchange.pathMatchers("/","/*.css","/*.js","/favicon.ico").permitAll() // SPA의 정적 리소스에 대한 인증되지 않은 액세스 허용
+                        .pathMatchers(HttpMethod.GET,"/books/**").permitAll() // 카탈로그의 도서에 대한 인증되지 않은 액세스 허용
+                        .anyExchange().authenticated())
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(
+                        new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
+                ))
                 .oauth2Login(Customizer.withDefaults()) // OAuth2/OIDC를 사용한 사용자 인증을 활성화한다
                 .logout(logout -> logout.logoutSuccessHandler(
-                        oidcLogoutSuccessHandler(clientRegistrationRepository)))
-                .build();
+                        oidcLogoutSuccessHandler(clientRegistrationRepository)));
 
         return http.build();
     }
